4b44b178bb128d6217f5a9a379022f818db5b7ef
agic M <1836167892@qq.com>
2022-05-25 21:44:27 +0800

    修改数据库连接的密码

--- WebApi/Models/StudentsContext.cs
+++ WebApi/Models/StudentsContext.cs
@@ -25,7 +25,7 @@ namespace WebApi.Models
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data source=.;Initial Catalog=Students;User ID=sa;Password=123456;Encrypt=True;TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer("Data source=.;Initial Catalog=Students;User ID=sa;Password=sql1/0.618data;Encrypt=True;TrustServerCertificate=True");
             }
         }
 
